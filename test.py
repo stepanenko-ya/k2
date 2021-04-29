b52900371184b35908a50e93177c4afa27acb09c
stepanenko-ya <stepanenko.yana.87@gmail.com>
2021-04-29 16:38:25 +0300

    k2

---
+++ test.py
@@ -0,0 +1,14 @@
+# _______________________________________________
+# pagination
+# x = 'https://k2.com.pl/produkty/kosmetyki?page=16'
+# pages = x.split("=")[1]
+# print(int(pages))
+# _______________________________________________
+import time
+import random
+
+print("time")
+q = random.uniform(1, 6)
+print(q)
+time.sleep(q)
+print("sleep")
